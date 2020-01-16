6bcc0d3204bd7857289d69304a33b2240c0248df
Lucas Pessone <lucas.rmagalhaes@gmail.com>
2020-01-16 13:05:32 -0200

    Add Routes and StatusBar controller

--- App.js
+++ App.js
@@ -1,25 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'react-native';
+import Routes from './src/routes';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Hello Reat Native!</Text>
-    </View>
+    <>
+      <StatusBar barStyle='light-content' backgroundColor='#7d40e7' />
+      <Routes />
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#515151',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-  title: {
-    fontWeight: 'bold',
-    color: '#fff',
-    fontSize: 32,
-  },
-});
